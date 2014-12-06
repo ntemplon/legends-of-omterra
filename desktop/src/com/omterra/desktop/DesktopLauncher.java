50762b53c659ebb5f1d4c2e5f9b112a33f283a2b
nathan <nathan@nathan-mint>
2014-12-05 19:54:38 -0600

    Initial Commit

---
+++ desktop/src/com/omterra/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.omterra.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.omterra.OmterraGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OmterraGame(), config);
+	}
+}
